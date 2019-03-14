55af7456773b2cbf6cd645a16ba95c1e8a3e9023
JMONDESIR <Joey.M.Dev@gmail.com>
2019-03-14 15:59:06 -0500

    stashed

--- src/scripts/dom-manager.js
+++ src/scripts/dom-manager.js
@@ -1,26 +1,26 @@
 //three DOM contructor functions
 
 const buildElementWithText = (elementType, elementTextContent, elementID) => {
-  let htmlElement = document.createElement(elementType);
-  htmlElement.textContent = elementTextContent;
-  htmlElement.id = elementID;
-  return htmlElement;
+        let htmlElement = document.createElement(elementType);
+        htmlElement.textContent = elementTextContent;
+        htmlElement.id = elementID;
+        return htmlElement;
 };
 
 const buildInputElement = (elementType, elementId) => {
-  let htmlElement = document.createElement("input");
-  htmlElement.type = elementType;
-  htmlElement.id = elementId;
-  return htmlElement;
+        let htmlElement = document.createElement("input");
+        htmlElement.type = elementType;
+        htmlElement.id = elementId;
+        return htmlElement;
 };
 
 const buildButtonElement = (elementId, elementText, elementClass) => {
-  let button = document.createElement("button");
-  button.id = elementId;
-  button.setAttribute("type", "submit");
-  button.textContent = elementText;
-  button.classList.add(elementClass);
-  return button;
+        let button = document.createElement("button");
+        button.id = elementId;
+        button.setAttribute("type", "submit");
+        button.textContent = elementText;
+        button.classList.add(elementClass);
+        return button;
 }
 
 
@@ -28,53 +28,53 @@ const buildButtonElement = (elementId, elementText, elementClass) => {
 const mainCont = document.querySelector(".main-container");
 //function that creates the template for building input fields 
 const fieldsetCreator = (itItem) => {
-  const formFieldSet = (buildElementWithText("fieldset"));
-  formFieldSet.appendChild(buildElementWithText("label", `Search for ${itItem}s: `));
-  formFieldSet.appendChild(buildInputElement("text", `${itItem}Input`));
-  formFieldSet.appendChild(buildButtonElement(`${itItem}Button`, "Search", "searchButton"));
-  // formFieldSet.appendChild(parkSelectElement())
-  return formFieldSet;
+        const formFieldSet = (buildElementWithText("fieldset"));
+        formFieldSet.appendChild(buildElementWithText("label", `Search for ${itItem}s: `));
+        formFieldSet.appendChild(buildInputElement("text", `${itItem}Input`));
+        formFieldSet.appendChild(buildButtonElement(`${itItem}Button`, "Search", "searchButton"));
+        // formFieldSet.appendChild(parkSelectElement())
+        return formFieldSet;
 };
 
 //function that creates the input form section and append to DOM
 const buildInputForm = () => {
-  const formSection = document.createElement("form");
-  formSection.id = "form-container";
-  formSection.setAttribute("onsubmit", "return false");
-  formSection.appendChild(fieldsetCreator1("park"));
-  formSection.appendChild(fieldsetCreator("restaurant"));
-  formSection.appendChild(fieldsetCreator("event"));
-  formSection.appendChild(fieldsetCreator("concert"));
-  // formSection.appendChild(parkSelectElement())
-  mainCont.appendChild(formSection);
+        const formSection = document.createElement("form");
+        formSection.id = "form-container";
+        formSection.setAttribute("onsubmit", "return false");
+        formSection.appendChild(fieldsetCreator1("park"));
+        formSection.appendChild(fieldsetCreator("restaurant"));
+        formSection.appendChild(fieldsetCreator("event"));
+        formSection.appendChild(fieldsetCreator("concert"));
+        // formSection.appendChild(parkSelectElement())
+        mainCont.appendChild(formSection);
 };
 
 //function to create initial HTML structures
 const createDOM = () => {
-  mainCont.appendChild(buildElementWithText("h1", "Nashville Itinerary Planner"));
-  buildInputForm();
+        mainCont.appendChild(buildElementWithText("h1", "Nashville Itinerary Planner"));
+        buildInputForm();
 
-  mainCont.appendChild(buildElementWithText("section", "", "search-container"));
-  mainCont.appendChild(buildElementWithText("section", "", "itinerary-container"));
+        mainCont.appendChild(buildElementWithText("section", "", "search-container"));
+        mainCont.appendChild(buildElementWithText("section", "", "itinerary-container"));
 };
 
 
 /*function that accepts an array of arrays, creates the structure for the search results, and appends them to the dom when the search button is clicked.*/
 const searchResultsObject = (array, API) => {
-  const searchContainer = document.querySelector("#search-container");
-  while (searchContainer.firstChild) {
-    searchContainer.removeChild(searchContainer.firstChild);
-  }
-  const createResultList = (buildElementWithText("ol", "Click item to add:", API));
-  array.forEach((obj, index) => {
-    const createListItem = buildElementWithText("li", obj.join(" : "), `${API}Item${index}`);
-    createListItem.classList.add(API)
-    const createSpan = buildElementWithText("span", "", index);
-    createSpan.appendChild(createListItem);
+        const searchContainer = document.querySelector("#search-container");
+        while (searchContainer.firstChild) {
+                searchContainer.removeChild(searchContainer.firstChild);
+        }
+        const createResultList = (buildElementWithText("ol", "Click item to add:", API));
+        array.forEach((obj, index) => {
+                const createListItem = buildElementWithText("li", obj.join(" : "), `${API}Item${index}`);
+                createListItem.classList.add(API)
+                const createSpan = buildElementWithText("span", "", index);
+                createSpan.appendChild(createListItem);
 
-    createResultList.appendChild(createSpan);
-  });
-  searchContainer.appendChild(createResultList);
+                createResultList.appendChild(createSpan);
+        });
+        searchContainer.appendChild(createResultList);
 }
 
 
--- src/scripts/restaurant-event-handler.js
+++ src/scripts/restaurant-event-handler.js
@@ -1,38 +1,81 @@
-// const searchButton = document.querySelector("#restaurantButton")
-// const restaurantInput = document.querySelector("#restaurantInput")
-// const key = "f86bf5c13d03c028aafe4818c8c4aa41"
 
 
-// searchButton.addEventListener("click", () => {
-//         console.log(restaurantInput.value)
-//         fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&q=${restaurantInput.value}`, {
-//                 headers: {
-//                         "Accept": "application/json",
-//                         "user-key": "f86bf5c13d03c028aafe4818c8c4aa41"
-//                 }
-//         })
 
-//                 .then(results => results.json())
-//                 .then(results => {
-//                         console.log(results)
-//                         console.log(results.restaurants)
-//                 })
-// })
 
-const getRestaurantAPI = (keyword) => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const key = "f86bf5c13d03c028aafe4818c8c4aa41"
+
+const getRestaurantAPI = (restaurantInputValue) => {
         return fetch(
-                `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&q=${keyword}`, {
+                `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&q=${restaurantInputValue}`, {
                         headers: {
                                 Accept: "application/json",
-                                "user-key": "8ee4b852db1b6e2be24fa391c91541db"
+                                "user-key": key
                         }
                 }
         )
                 .then(r => r.json())
                 .then(results => {
                         const restResults = results.restaurants
+                        console.log(restaurantInputValue)
+                        console.log(results)
                         const restArray = restResults.map(details => {
                                 const restCombo = details.restaurant;
+                                console.log(details)
                                 return restCombo;
                         }); return restArray
                 }).then(data => {
