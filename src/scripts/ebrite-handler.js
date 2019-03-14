e9d1763e16c5cdc89a51a35c036dc82b825ed0a7
JMONDESIR <Joey.M.Dev@gmail.com>
2019-03-14 10:43:05 -0500

    stashed

--- src/scripts/ebrite-handler.js
+++ src/scripts/ebrite-handler.js
@@ -3,14 +3,14 @@
 
 const eventSearch = document.querySelector("#eventInput")
 const eventButton = document.querySelector("#eventButton")
-eventButton.addEventListener("click", ()=> {
-   //console.log(eventSearch.value)
+eventButton.addEventListener("click", () => {
+
    fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${eventSearch.value}&location.address=nashville&token=5Q3DYW2NIIHRZTV4OJ6Z`, {
       headers: {
          Accept: "application/json"
       }
    }).then(response => response.json())
-   .then(results=>{
-       console.log(results)
-   })
+      .then(results => {
+         console.log(results)
+      })
 })
