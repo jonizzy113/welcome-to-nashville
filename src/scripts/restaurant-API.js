4b4e5e30b5e6baeac47af66c528eec10de7ebb7b
Brian Jobe <47683894+greenskybluephish@users.noreply.github.com>
2019-03-14 11:38:54 -0500

    Update and rename restaurant-event-handler.js to restaurant-API.js

--- src/scripts/restaurant-API.js
+++ src/scripts/restaurant-API.js
@@ -1,23 +1,3 @@
-// const searchButton = document.querySelector("#restaurantButton")
-// const restaurantInput = document.querySelector("#restaurantInput")
-// const key = "f86bf5c13d03c028aafe4818c8c4aa41"
-
-
-// searchButton.addEventListener("click", () => {
-//         console.log(restaurantInput.value)
-//         fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&q=${restaurantInput.value}`, {
-//                 headers: {
-//                         "Accept": "application/json",
-//                         "user-key": "f86bf5c13d03c028aafe4818c8c4aa41"
-//                 }
-//         })
-
-//                 .then(results => results.json())
-//                 .then(results => {
-//                         console.log(results)
-//                         console.log(results.restaurants)
-//                 })
-// })
 
 const getRestaurantAPI = (keyword) => {
         return fetch(
