e63b4bd68f19042a55bde14d38ecf02d080666f8
JMONDESIR <Joey.M.Dev@gmail.com>
2019-03-14 10:48:34 -0500

    stashed

--- src/scripts/restaraunt-event-handler.js
+++ src/scripts/restaraunt-event-handler.js
@@ -1,20 +1,44 @@
-const searchButton = document.querySelector("#restaurantButton")
-const restaurantInput = document.querySelector("#restaurantInput")
-const key = "f86bf5c13d03c028aafe4818c8c4aa41"
+// const searchButton = document.querySelector("#restaurantButton")
+// const restaurantInput = document.querySelector("#restaurantInput")
+// const key = "f86bf5c13d03c028aafe4818c8c4aa41"
 
 
-searchButton.addEventListener("click", () => {
-        console.log(restaurantInput.value)
-        fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&q=${restaurantInput.value}`, {
-                headers: {
-                        "Accept": "application/json",
-                        "user-key": "f86bf5c13d03c028aafe4818c8c4aa41"
+// searchButton.addEventListener("click", () => {
+//         console.log(restaurantInput.value)
+//         fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&q=${restaurantInput.value}`, {
+//                 headers: {
+//                         "Accept": "application/json",
+//                         "user-key": "f86bf5c13d03c028aafe4818c8c4aa41"
+//                 }
+//         })
+
+//                 .then(results => results.json())
+//                 .then(results => {
+//                         console.log(results)
+//                         console.log(results.restaurants)
+//                 })
+// })
+
+const getRestaurantAPI = (keyword) => {
+        return fetch(
+                `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&q=${keyword}`, {
+                        headers: {
+                                Accept: "application/json",
+                                "user-key": "f86bf5c13d03c028aafe4818c8c4aa41"
+                        }
                 }
-        })
-
-                .then(results => results.json())
+        )
+                .then(r => r.json())
                 .then(results => {
-                        console.log(results)
-                        console.log(results.restaurants)
+                        const restResults = results.restaurants
+                        const restArray = restResults.map(details => {
+                                const restCombo = details.restaurant;
+                                return restCombo;
+                        }); return restArray
+                }).then(data => {
+                        const comboData = data.map(info => {
+                                comboInfo = [info.name, info.location.address]
+                                return comboInfo;
+                        }); return comboData;
                 })
-})
+};
