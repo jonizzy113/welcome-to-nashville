80697536be7b84055a226bd2840e7aba2267966d
JMONDESIR <Joey.M.Dev@gmail.com>
2019-03-15 09:24:13 -0500

    2nd final commit

--- src/scripts/parksapi.js
+++ src/scripts/parksapi.js
@@ -1,33 +1,3 @@
-
-const fieldsetCreator1 = (itItem) => {
-        const formFieldSet1 = (buildElementWithText("fieldset"));
-        formFieldSet1.appendChild(buildElementWithText("label", `Search for ${itItem}s: `));
-        formFieldSet1.appendChild(parkSelectElement());
-        formFieldSet1.appendChild(buildButtonElement(`${itItem}Button`, "Search", "searchButton"));
-        return formFieldSet1;
-};
-
-buildOptionElement = (value, text, id) => {
-        const option = document.createElement("option");
-        option.value = value;
-        option.textContent = text;
-        return option
-
-}
-
-const parkSelectElement = () => {
-        const formSelect = document.createElement("select");
-        formSelect.id = "park"
-        formSelect.appendChild(buildOptionElement("dog_park", "Dog Park"));
-        formSelect.appendChild(buildOptionElement("boat_launch", "Boat Launch"));
-        formSelect.appendChild(buildOptionElement("skate_park", "Skate Park"));
-        formSelect.appendChild(buildOptionElement("volleyball", "Volleyball"));
-        formSelect.appendChild(buildOptionElement("tennis_courts", "Tennis Court"));
-        return formSelect;
-};
-
-
-
 const getParksAPI = (park1) => {
         console.log(park1)
         return fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?${park1}=Yes`)
