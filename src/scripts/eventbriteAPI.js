2329c290eb8870aaeae2ba24c6ec96a8265a5ff0
Josh Hibray <jjhibray@gmail.com>
2019-03-15 09:09:29 -0500

    recreated eventbrite api while understanding the process of fetching, organizing and returning the data from the eventbrite api

--- src/scripts/eventbriteAPI.js
+++ src/scripts/eventbriteAPI.js
@@ -1,5 +1,38 @@
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const getEventAPI = (keyword) => {
   return fetch(
      `https://www.eventbriteapi.com/v3/events/search/?q=${keyword}&location.address=nashville&expand=venue&token=PWTYFCY4EA2D7ZA3SVWG`, {
@@ -9,14 +42,21 @@ const getEventAPI = (keyword) => {
      }
   ).then(r => r.json()).then(results => {
      const events = results.events;
+    // console.log(events);   Test results from event object as well as get in to event area because .map only works with arrays - JH
      return events
+     
   }).then(data => {
      const eventsCombo = data.map(info => {
         const combolist = [info.name.text, info.venue.address.localized_address_display]
+       // console.log(combolist)  testing it is bringing out the two correct properties from the array I am trying to show for the data parameter - JH
         return combolist;
-     });
+     }); 
      return eventsCombo
   }).then(data => {
      return data
   })
 };
+
+getEventAPI("meetups");
+
+
